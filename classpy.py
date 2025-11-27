9ad66e2ee0b0e6ff4ae2cbd3e4066555dac6ca70
Sekhar531 <krajasekha531@gmail.com>
2025-11-27 18:10:37 +0530

    python script

--- classpy.py
+++ classpy.py
@@ -1,6 +1,30 @@
-class Dog:
-    def __init__(self,name):
+import string
+class student:
+    def __init__(self,name,age,grade):
         self.name = name
-        print("Mydog name is: " + name)
+        self.age = age
+        self.grade = grade
+    def get_grade(self):
+        return self.grade
+class course:
+    def __init__(self,name,maxstudents):
+        self.name = name
+        self.maxstudents = maxstudents
+        self.students = []
+    def add_student(self,student):
+        if len(self.student) < self.maxstudents:
+            self.students.append(student)
+            return True
+        return False
+    def get_average_grade(self):
+        pass
 
-d = Dog("Jojo")
+s1 = student("subbu",17,90)
+s2 = student("Ram",17,76)
+s3 = student("subbu",17,85)
+
+cs = course("science",2)
+cs.add_student(s1)
+cs.add_student(s2)
+
+print(cs.students)
